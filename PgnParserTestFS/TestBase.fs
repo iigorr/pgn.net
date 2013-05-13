6e9d31e8f8b032e27b130ce9a46245a723c0d6d3
Igor Lankin <dev@lankin.net>
2013-05-14 00:25:17 +0200

    - added shouldFail to TestBase functions

--- PgnParserTestFS/TestBase.fs
+++ PgnParserTestFS/TestBase.fs
@@ -4,6 +4,8 @@ open FParsec
 open ilf.pgn
 open ilf.pgn.PgnParsers
 
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
 let parse p str =
     match run p str with
     | Success(result, _, _)   -> result
@@ -13,3 +15,8 @@ let tryParse p str =
     match run p str with
     | Success(result, _, _)   -> ()
     | Failure(errorMsg, _, _) -> raise (ParseException errorMsg)
+
+let shouldFail p str = 
+    match run p str with
+    | Success(result, _, _)   -> raise (AssertFailedException "Expected parser did not fail")
+    | Failure(errorMsg, _, _) -> ()
