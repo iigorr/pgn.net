44b8b2d784446da351c849b8d75c41a15dea61ad
Igor Lankin <dev@lankin.net>
2013-05-14 00:23:18 +0200

    - added debugging functions - added strCI parser (case insensitive)

--- PgnParser/BasicParsers.fs
+++ PgnParser/BasicParsers.fs
@@ -4,4 +4,17 @@ open FParsec
 
 let ws = spaces
 let str = pstring
+let strCI = pstringCI
 let pNotChar c = manySatisfy (fun x -> x <> c)
+
+let pList(p, list:'a list) = list |> List.map p |> choice
+
+let BP (p: Parser<_,_>) stream =
+    p stream // set a breakpoint here
+
+let NBP (p: Parser<_,_>, name:string) stream =
+    p stream // set a breakpoint here
+
+let D (p: Parser<_,_>, name:string) stream =
+    System.Console.WriteLine(name);
+    p stream
