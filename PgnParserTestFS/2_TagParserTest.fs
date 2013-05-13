e4b04add7943cc0b2216134af85bf2b133a60051
Igor Lankin <dev@lankin.net>
2013-05-13 15:32:33 +0200

    - renbamed 2_Tests to 2_TagParserTest

--- PgnParserTestFS/2_TagParserTest.fs
+++ PgnParserTestFS/2_TagParserTest.fs
@@ -27,3 +27,27 @@ type ParserTests() =
         tryParse pTag "[Date \t \n   \t\"Foo\"]"
         tryParse pTag "[  \t \nDate         \"Foo\"\t \n ]  \t \n   \t \n"
         tryParse pTag "  \t \n  [  \t \n   \t \n Date \t \n   \t \n  \"Foo\"  \t \n   \t \n ]  \t\n\t \n   \t\t\n\n   \t \n\n "
+
+    [<TestMethod>]
+    ///<see href="see http://www.saremba.de/chessgml/standards/pgn/pgn-complete.htm#c8.1.1" />
+    member this.pTag_should_allow_tag_names_from_SevenTagRoaster() =
+        let allowedTagNames = ["Event"; "Site"; "Date"; "Round"; "White"; "Black"; "Result"]
+        let parseTag x = tryParse pTag ("["+ x + " \"Foo\"]")
+        List.map parseTag allowedTagNames |> ignore
+        ()
+
+    [<TestMethod>]
+    ///<see href="http://www.saremba.de/chessgml/standards/pgn/pgn-complete.htm#c9" />
+    member this.pTag_should_allow_suplemental_tag_names() =
+        let allowedTagNames = 
+            ["WhiteTitle"; "BlackTitle"; "WhiteElo"; "BlackElo"; "WhiteUSCF"; "BlackUSCF"; "WhiteNA"; "BlackNA"; "WhiteType"; "BlackType"; 
+            "EventDate"; "EventSponsor"; "Section"; "Stage"; "Board";
+            "Opening"; "Variation"; "SubVariation";
+            "ECO"; "NIC"; "Time"; "UTCTime"; "UTCDate";
+            "TimeControl";
+            "SetUp"; "FEN";
+            "Termination";
+            "Annotator"; "Mode"; "PlyCount"]
+        let parseTag x = tryParse pTag ("["+ x + " \"Foo\"]")
+        List.map parseTag allowedTagNames |> ignore
+        ()
