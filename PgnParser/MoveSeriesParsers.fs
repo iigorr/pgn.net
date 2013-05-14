ed132b1edb1ca8097f0052d2ca8e17f2f72a794f
Igor Lankin <dev@lankin.net>
2013-05-14 02:19:25 +0200

    - added initial parsers for move series

---
+++ PgnParser/MoveSeriesParsers.fs
@@ -0,0 +1,16 @@
+﻿module ilf.pgn.PgnParsers.MoveSeries
+
+open FParsec
+open ilf.pgn.PgnParsers.Basic
+open ilf.pgn.PgnParsers.Move
+
+let pPeriods = (many1Chars (pchar '.')) <|> str "…"
+
+let pMoveNumberIndicator = pint32 .>> ws .>> pPeriods <!> "pMoveNumberIndicator"
+let pFinishedMovePair =
+    opt pMoveNumberIndicator .>> ws1 .>> pMove .>> ws1 .>> pMove <!> "pFinishedMovePair"
+let pUnfinishedMovePair = 
+    opt pMoveNumberIndicator .>> ws1 .>> pMove <!> "pUnfinishedMovePair"
+
+let pMovePair= attempt(pFinishedMovePair) <|> (pUnfinishedMovePair .>> ws .>> pUnfinishedMovePair) <!> "pMovePair"
+let appyPMoveSeries (p: string)= run pMovePair p
---
+++ PgnParserTestFS/MoveSieriesParserTests.fs
@@ -0,0 +1,37 @@
+﻿namespace ilf.pgn.Test
+
+open ilf.pgn
+open ilf.pgn.PgnParsers.MoveSeries
+open ilf.pgn.Test.TestBase
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type MoveSeriesParserTest() = 
+    [<TestMethod>]
+    member this.pMoveNumberIndicator_should_accept_simple_move_number() =
+        tryParse pMoveNumberIndicator "1."
+        tryParse pMoveNumberIndicator "104."
+
+    [<TestMethod>]
+    member this.pMoveNumberIndicator_should_accept_continued_move_number() =
+        tryParse pMoveNumberIndicator "2..."
+        tryParse pMoveNumberIndicator "54....."
+        tryParse pMoveNumberIndicator "7…"
+
+    [<TestMethod>]
+    member this.pMoveNumberIndicator_should_accept_spaces_between_number_and_periods() =
+        tryParse pMoveNumberIndicator "2  \t..."
+        tryParse pMoveNumberIndicator "1    ."
+        tryParse pMoveNumberIndicator "54\n....."
+        tryParse pMoveNumberIndicator "7 …"
+
+    [<TestMethod>]
+    member this.pMovePair_should_accept_move_pair() =
+        tryParse pMovePair "1. e2e4 Nb8c6"
+        tryParse pMovePair "1 .   e2e4   Nb8c6"    
+    
+    [<TestMethod>]
+    member this.pMovePair_should_accept_a_split_move() =
+        tryParse pMovePair "1. e2e4 \n 1... Nb8c6"
+
