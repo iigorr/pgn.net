86f675a45544201ff4a5d730e9f5fbd728791f14
Igor Lankin <dev@lankin.net>
2013-05-17 03:46:03 +0200

    - implemented equals on move

--- pgn.net/Move.fs
+++ pgn.net/Move.fs
@@ -20,3 +20,22 @@ type Move() =
     member val OriginFile : File option = None with get, set
     member val OriginRank : int option = None with get, set
     member val PromotedPiece : Piece option = None with get, set
+
+    override x.Equals(yobj) =
+        match yobj with
+        | :? Move as y -> x.EqualsMove(y)
+        | :? Option<Move> as y -> 
+            match y with
+            | None -> false
+            | Some(move) -> x.EqualsMove(move)
+        | _ -> false
+
+    member x.EqualsMove(y : Move) = 
+            (x.Type = y.Type) 
+            && (x.TargetPiece = y.TargetPiece)
+            && (x.TargetSquare = y.TargetSquare)
+            && (x.Piece = y.Piece)
+            && (x.OriginSquare = y.OriginSquare)
+            && (x.OriginFile = y.OriginFile)
+            && (x.OriginRank = y.OriginRank)
+            && (x.PromotedPiece = y.PromotedPiece)
