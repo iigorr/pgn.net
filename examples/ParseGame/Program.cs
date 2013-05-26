b2740ec5bb76ad1204f01ff3e315d5f055941310
Igor Lankin <dev@lankin.net>
2013-05-26 02:02:23 +0200

    - added simple example solution

---
+++ examples/ParseGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ParseGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            var parser = new ilf.pgn.Parser();
+            var gameDb = parser.ReadFromFile("Tarrasch.pgn");
+
+            var game = gameDb.Games[0];
+
+            Console.WriteLine("Game " + game.WhitePlayer + " vs. " + game.BlackPlayer);
+            Console.WriteLine(game.Result);
+        }
+    }
+}
