3f757379503ba43c65d567c2687e2d6fd7155313
Igor Lankin <dev@lankin.net>
2013-06-01 01:50:03 +0200

    - implemented ToString of GameInfo

--- src/pgn.Data/GameInfo.cs
+++ src/pgn.Data/GameInfo.cs
@@ -10,6 +10,11 @@
 
         public string Name { get; set; }
         public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("[{0} \"{1}\"]", Name, Value);
+        }
     }
 
 }
