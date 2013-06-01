ca13bce0600a250d8331a0edf0b528bb4e9cbb3e
Igor Lankin <dev@lankin.net>
2013-06-01 02:24:57 +0200

    - implemented formatting of games

---
+++ src/pgn.Data.Test/FormatterTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ilf.pgn.Data.Format.Test
+{
+    [TestClass]
+    public class FormatterTest
+    {
+        private readonly Game _testGame;
+        private const string TestGameString = 
+@"[Event ""Breslau""]
+[Site ""Breslau""]
+[Date ""1879.??.??""]
+[Round ""?""]
+[White ""Tarrasch, Siegbert""]
+[Black ""Mendelsohn, J.""]
+[Result ""1-0""]
+
+{some moves} 1-0";
+
+        public FormatterTest()
+        {
+            _testGame = new Game()
+                {
+                    Event = "Breslau",
+                    Site = "Breslau",
+                    Year = 1879,
+                    WhitePlayer = "Tarrasch, Siegbert",
+                    BlackPlayer = "Mendelsohn, J.",
+                    Result = GameResult.White,
+                };
+            _testGame.MoveText = new List<MoveTextEntry> { new CommentEntry("some moves"), new GameEndEntry(GameResult.White) };
+        }
+        [TestMethod]
+        public void can_construct()
+        {
+            new Formatter();
+        }
+
+        [TestMethod]
+        public void Format_should_accept_TextWriter()
+        {
+            var sut = new Formatter();
+            var writer = new StringWriter();
+            writer.Write("Foo ");
+            sut.Format(_testGame, writer);
+            Assert.AreEqual("Foo " + TestGameString, writer.ToString());
+        }
+
+        [TestMethod]
+        public void Format_should_format_correctly()
+        {
+            var sut = new Formatter();
+            Assert.AreEqual(TestGameString, sut.Format(_testGame));
+        }
+    }
+}
---
+++ src/pgn.Data/Format/Formatter.cs
@@ -0,0 +1,64 @@
+﻿using System.Globalization;
+using System.IO;
+
+namespace ilf.pgn.Data.Format
+{
+    public class Formatter
+    {
+        private MoveTextFormatter _moveTextFormatter = new MoveTextFormatter();
+        public string Format(Game game)
+        {
+            var writer = new StringWriter();
+            Format(game, writer);
+            return writer.ToString();
+        }
+
+        public void Format(Game game, TextWriter writer)
+        {
+            FormatTag("Event", game.Event, writer);
+            FormatTag("Site", game.Site, writer);
+            FormatDate(game, writer);
+            FormatTag("Round", game.Round, writer);
+            FormatTag("White", game.WhitePlayer, writer);
+            FormatTag("Black", game.BlackPlayer, writer);
+            FormatTag("Result", GetResultString(game.Result), writer);
+
+            foreach(var info in game.AdditionalInfo)
+                FormatTag(info.Name, info.Value, writer);
+
+            writer.WriteLine();
+            _moveTextFormatter.Format(game.MoveText, writer);
+        }
+
+        private void FormatDate(Game game, TextWriter writer)
+        {
+            writer.Write("[Date \"");
+            writer.Write(game.Year == null ? "????" : game.Year.Value.ToString(CultureInfo.InvariantCulture));
+            writer.Write(".");
+            writer.Write(game.Month == null ? "??" : game.Month.Value.ToString(CultureInfo.InvariantCulture));
+            writer.Write(".");
+            writer.Write(game.Day == null ? "??" : game.Day.Value.ToString(CultureInfo.InvariantCulture));
+            writer.WriteLine("\"]");
+        }
+
+        private string GetResultString(GameResult result)
+        {
+            switch (result)
+            {
+                case GameResult.White: return "1-0";
+                case GameResult.Black: return "0-1";
+                case GameResult.Draw: return "½-½";
+            }
+            return "*";
+        }
+
+
+        private void FormatTag(string name, object value, TextWriter writer)
+        {
+            writer.Write("[");
+            writer.Write(name + " \"");
+            writer.Write(value ?? "?");
+            writer.WriteLine("\"]");
+        }
+    }
+}
