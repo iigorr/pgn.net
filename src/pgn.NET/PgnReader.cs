989811d9b8ac06a8cda4ccd07ab202b65f36866a
Igor Lankin <dev@lankin.de>
2013-07-04 01:20:48 +0200

    - added documentation to pgn reader and writer.

--- src/pgn.NET/PgnReader.cs
+++ src/pgn.NET/PgnReader.cs
@@ -4,20 +4,38 @@ using ilf.pgn.PgnParsers;
 
 namespace ilf.pgn
 {
+    /// <summary>
+    /// Pgn format reader.
+    /// </summary>
     public class PgnReader
     {
-        public Database ReadFromFile(string file)
+        /// <summary>
+        /// Reads a pgn database from a fileName.
+        /// </summary>
+        /// <param name="fileName">The fileName name.</param>
+        /// <returns>A pgn database.</returns>
+        public Database ReadFromFile(string fileName)
         {
             var p = new Parser();
-            return p.ReadFromFile(file);
+            return p.ReadFromFile(fileName);
         }
 
+        /// <summary>
+        /// Reads a pgn database from a stream.
+        /// </summary>
+        /// <param name="stream">The stream.</param>
+        /// <returns>A pgn database.</returns>
         public Database ReadFromStream(Stream stream)
         {
             var p = new Parser();
             return p.ReadFromStream(stream);
         }
 
+        /// <summary>
+        /// Reads a pgn database from a string.
+        /// </summary>
+        /// <param name="input">The string input.</param>
+        /// <returns>A pgn database.</returns>
         public Database ReadFromString(string input)
         {
             var p = new Parser();
--- src/pgn.NET/PgnWriter.cs
+++ src/pgn.NET/PgnWriter.cs
@@ -4,25 +4,40 @@ using ilf.pgn.Data.Format;
 
 namespace ilf.pgn
 {
+    /// <summary>
+    /// Pgn format writer.
+    /// </summary>
     public class PgnWriter
     {
         private readonly Stream _stream;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PgnWriter"/>.
+        /// </summary>
+        /// <param name="stream">A stream to write to.</param>
         public PgnWriter(Stream stream)
         {
             _stream = stream;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PgnWriter"/>.
+        /// </summary>
+        /// <param name="fileName">Name of the file to write to.</param>
         public PgnWriter(string fileName)
         {
             _stream=new FileStream(fileName, FileMode.OpenOrCreate);
         }
 
-        public void Write(Database db)
+        /// <summary>
+        /// Writes the specified pgn database.
+        /// </summary>
+        /// <param name="pgnDatabase">The pgn database.</param>
+        public void Write(Database pgnDatabase)
         {
             var formatter = new Formatter();
 
             TextWriter writer = new StreamWriter(_stream);
-            foreach (var game in db.Games)
+            foreach (var game in pgnDatabase.Games)
             {
                 formatter.Format(game, writer);
             }
