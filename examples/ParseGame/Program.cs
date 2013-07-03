e83e3828d63ebdee3601256a6546c144a22b0bcb
Igor Lankin <dev@lankin.de>
2013-07-04 00:51:16 +0200

    - updated parse example project

--- examples/ParseGame/Program.cs
+++ examples/ParseGame/Program.cs
@@ -7,13 +7,12 @@ namespace ParseGame
     {
         static void Main()
         {
-            var parser = new ilf.pgn.Parser();
-            var gameDb = parser.ReadFromFile("Tarrasch.pgn");
+            var reader = new ilf.pgn.PgnReader();
+            var gameDb = reader.ReadFromFile("Tarrasch.pgn");
 
             Game game = gameDb.Games[0];
 
-            Console.WriteLine("Game " + game.WhitePlayer + " vs. " + game.BlackPlayer);
-            Console.WriteLine("Game result: " + game.Result + " in Round " + (game.Round??"?"));
+            Console.WriteLine(game);
 
             Console.ReadKey();
         }
