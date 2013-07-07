a516f45d8a4c374867d632a4579f9e7f95513203
Igor Lankin <dev@lankin.de>
2013-07-07 12:54:42 +0200

    - added docs for GameInfo

--- src/pgn.Data/GameInfo.cs
+++ src/pgn.Data/GameInfo.cs
@@ -1,16 +1,42 @@
 ﻿namespace ilf.pgn.Data
 {
+    /// <summary>
+    /// Represents additional game information (usually a tag).
+    /// </summary>
     public class GameInfo
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameInfo"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="value">The value.</param>
         public GameInfo(string name, string value)
         {
             Name = name;
             Value = value;
         }
 
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
         public string Value { get; set; }
 
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
         public override string ToString()
         {
             return string.Format("[{0} \"{1}\"]", Name, Value);
