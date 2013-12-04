bafe51be247d501e36457dc96ea4fec58a2885aa
Igor Lankin <dev@lankin.net>
2013-12-04 23:43:11 +0100

    add MoveTextEntryList class

---
+++ src/pgn.Data.Test/MoveTextEntryListTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ilf.pgn.Data.MoveText;
+
+namespace ilf.pgn.Data.Test
+{
+    [TestClass]
+    public class MoveTextEntryListTest
+    {
+        [TestMethod]
+        public void Instance_should_be_a_list_of_MoveTextEntries()
+        {
+            Assert.IsInstanceOfType(new MoveTextEntryList(), typeof(List<MoveTextEntry>));
+        }
+
+        [TestMethod]
+        public void FullMoveCount_should_return_move_count_without_comments()
+        {
+            var sut = new MoveTextEntryList
+                {
+                    new CommentEntry("foo"),
+                    new NAGEntry(1),
+                    new RAVEntry(new List<MoveTextEntry>{new MovePairEntry(new Move(), new Move())}),
+                    new MovePairEntry(new Move(), new Move()),
+                    new MovePairEntry(new Move(), new Move()),
+                    new GameEndEntry(GameResult.Draw)
+                };
+
+            Assert.AreEqual(2, sut.FullMoveCount);
+            Assert.AreEqual(4, sut.HalfMoveCount);
+        }
+
+        [TestMethod]
+        public void FullMoveCount_should_not_count_single_halfmoves()
+        {
+            var sut = new MoveTextEntryList { new HalfMoveEntry(new Move()) };
+            Assert.AreEqual(0, sut.FullMoveCount);
+            Assert.AreEqual(1, sut.HalfMoveCount);
+
+        }
+
+        [TestMethod]
+        public void FullMoveCount_should_count_pairwise_halfmoves()
+        {
+            var sut = new MoveTextEntryList { new HalfMoveEntry(new Move()), new HalfMoveEntry(new Move()) };
+            Assert.AreEqual(1, sut.FullMoveCount);
+            Assert.AreEqual(2, sut.HalfMoveCount);
+        }
+
+    }
+}
---
+++ src/pgn.Data/MoveText/MoveTextEntryList.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ilf.pgn.Data.MoveText
+{
+    /// <summary>
+    /// List of MoveTextEntries which also provides some helpful
+    /// properties like MoveCount.
+    /// </summary>
+    public class MoveTextEntryList : List<MoveTextEntry>
+    {
+        /// <summary>
+        /// Gets the number of half-moves (moves by black *or* white)
+        /// </summary>
+        public int HalfMoveCount
+        {
+            get
+            {
+                int count = 0;
+                foreach (var entry in this)
+                {
+                    if (entry is MovePairEntry)
+                        count += 2;
+                    else if (entry is HalfMoveEntry)
+                        count += 1;
+                }
+
+                return count;
+            }
+        }
+
+        /// <summary>
+        /// Gets the number of full moves
+        /// </summary>
+        public int FullMoveCount
+        {
+            get { return HalfMoveCount / 2; }
+        }
+
+    }
+}
