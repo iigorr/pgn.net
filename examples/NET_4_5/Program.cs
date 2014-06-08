9ce6cf211df28f91e970f39ac3bbb7ccc076891b
Igor Lankin <dev@lankin.net>
2014-06-08 16:46:14 +0200

    added example projects for net4 and net45

---
+++ examples/NET_4_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ilf.pgn;
+using ilf.pgn.Data;
+
+namespace ReadPgnFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new PgnReader();
+
+            var db = reader.ReadFromFile("Tarrasch.pgn");
+            var game = db.Games[0];
+
+            Console.WriteLine("{0} vs {1} in {2} ({3})\n", game.WhitePlayer, game.BlackPlayer, game.Site, game.Year);
+
+
+            if (game.Result == GameResult.White)
+                Console.WriteLine("\nWhite wins in {0} moves.", game.MoveText.FullMoveCount);
+            else if (game.Result == GameResult.Black)
+                Console.WriteLine("\nBlack wins in {0} moves.", game.MoveText.FullMoveCount);
+            else if (game.Result == GameResult.Draw)
+                Console.WriteLine("\nGame ends in a draw.");
+            else
+                Console.WriteLine("\nGame not unfinshed.");
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
