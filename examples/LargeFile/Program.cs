311a51106a909a2faa261e855b2eeca2680ff870
Igor Lankin <dev@lankin.net>
2015-07-28 22:50:36 +0200

    updated examples and added a large-file example

---
+++ examples/LargeFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ilf.pgn;
+
+namespace ReadPgnFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new PgnReader();
+
+            var gamesCollection = reader.ReadGamesFromFile(@"c:\tmp\millionbase-2.22.pgn"); 
+
+            int count = 0;
+            var start = DateTime.Now;
+            foreach (var item in gamesCollection)
+            {
+                if (++count % 100 == 0)
+                {
+                    Console.WriteLine();
+                    var now = DateTime.Now;
+                    var timespan = now - start;
+                    var gamesPerSecond = ((double)count) / timespan.TotalSeconds;
+                    Console.SetCursorPosition(1, 0);
+                    Console.WriteLine("Time passed: {0}. Games: {1}, Games per Second: {2}", timespan, count, gamesPerSecond);
+                    if (count > 10000)
+                        break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
